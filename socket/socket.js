6512ed9825f0ed4dbce43da3c2c2e4fbe91f7d03
cyborkonline <davidmorais92@gmail.com>
2017-03-01 18:20:09 +0000

    App seems to be functional

--- socket/socket.js
+++ socket/socket.js
@@ -15,10 +15,75 @@ module.exports = function(io, rooms){
       socket.username = data.user;
       socket.userPic = data.userPic;
       socket.join(data.room);
+      updateUsersList(data.room, true);
   })
 
   socket.on('newMsg', function(data){
     socket.broadcast.to(data.room_number).emit('messagefeed', JSON.stringify(data));
   })
-})
-}
+  function findClientsSocket(roomId, namespace) {
+      var res = []
+      // the default namespace is "/"
+      , ns = io.of(namespace ||"/");
+
+      if (ns) {
+          for (var id in ns.connected) {
+              if(roomId) {
+                  var index = ns.connected[id].rooms.valueOf(roomId);
+                  if(index !== -1) {
+                      res.push(ns.connected[id]);
+                  }
+              } else {
+                  res.push(ns.connected[id]);
+              }
+          }
+      }
+      return res;
+  }
+
+  function updateUsersList(room, updateALL){
+    var getUsers = findClientsSocket(room, '/msgs');
+        var userslist = [];
+        for(var i in getUsers){
+          userslist.push({
+            user:getUsers[i].username,
+            userPic:getUsers[i].userPic
+          });
+        }
+        socket.to(room).emit('updateUsersList', JSON.stringify(userslist));
+
+        if(updateALL) {
+          socket.broadcast.to(room).emit('updateUsersList')
+        }
+
+      }
+
+      socket.on('updateList', function(data){
+        updateUsersList(data.room);
+      })
+
+    });
+
+  }
+    /*
+    From:
+    function findClientsSocket(roomId, namespace) {
+        var res = []
+        // the default namespace is "/"
+        , ns = io.of(namespace ||"/");
+
+        if (ns) {
+            for (var id in ns.connected) {
+                if(roomId) {
+                    var index = ns.connected[id].rooms.indexOf(roomId);
+                    if(index !== -1) {
+                        res.push(ns.connected[id]);
+                    }
+                } else {
+                    res.push(ns.connected[id]);
+                }
+            }
+        }
+        return res;
+    }
+*/
